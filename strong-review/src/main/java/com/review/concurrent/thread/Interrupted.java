19ce6c32b4127d1b9e1c06ab956592f2fda57fbc
qiaoyajun <1107830976@qq.com>
2020-03-12 00:40:58 +0800

    新增装饰器模式练习

---
+++ strong-review/src/main/java/com/review/concurrent/thread/Interrupted.java
@@ -0,0 +1,50 @@
+package com.review.concurrent.thread;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author: QiaoYaJun
+ * @Description
+ * @Created 2020-03-10 22:21
+ */
+public class Interrupted {
+    public static void main(String[] args) throws Exception {
+        // sleepThread不停的尝试睡眠
+        Thread sleepThread = new Thread(new SleepRunner(), "SleepThread");
+        sleepThread.setDaemon(true);
+        // busyThread不停的运行
+        Thread busyThread = new Thread(new BusyRunner(), "BusyThread");
+        busyThread.setDaemon(true);
+        sleepThread.start();
+        busyThread.start();
+        // 休眠5秒，让sleepThread和busyThread充分运行
+        TimeUnit.SECONDS.sleep(5);
+        sleepThread.interrupt();
+        busyThread.interrupt();
+        System.out.println("SleepThread interrupted is " + sleepThread.isInterrupted());
+        System.out.println("BusyThread interrupted is " + busyThread.isInterrupted());
+        // 防止sleepThread和busyThread立刻退出
+        TimeUnit.SECONDS.sleep(2);
+    }
+
+    static class SleepRunner implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    static class BusyRunner implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+            }
+        }
+    }
+}
